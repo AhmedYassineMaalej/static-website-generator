3bac621b1adfa80007c049242d7ca8b8e04a8001
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2026-01-30 10:42:07 +0100

    added LeafNode type

---
+++ src/leafnode.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+
+pub struct LeafNode {
+    tag: String,
+    value: String,
+    props: HashMap<String, String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod htmlnode;
+mod leafnode;
 mod textnode;
 
 use textnode::TextNode;
